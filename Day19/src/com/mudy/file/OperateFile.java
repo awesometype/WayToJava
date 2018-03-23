2723d266e7a5e4956d42bdfe6a17430373772e80
12东永 <2808050775@qq.com>
2018-03-23 15:38:32 +0800

    使用递归列出指定目录中的所有子文件

--- Day19/src/com/mudy/file/OperateFile.java
+++ Day19/src/com/mudy/file/OperateFile.java
@@ -1,11 +1,27 @@
 package com.mudy.file;
 
+import java.io.File;
+
 /**
  * Created by mudy on 18/3/23.
  * 文件操作
  */
 public class OperateFile {
     public static void main(String[] args) {
-
+        File dir = new File("/Users/mudy/IntelliJIDEAProjects/WayToJava");
+        listAllFile(dir);
     }
+
+    //列出指定文件夹下的所有文件，包括子文件夹下的文件 递归操作
+    private static void listAllFile(File file) {
+
+        for (File file1:file.listFiles()) {
+
+            if (file1.isDirectory()){//如果是目录，继续
+                listAllFile(file1);
+            }
+            System.out.println(file1);
+        }
+    }
+
 }
