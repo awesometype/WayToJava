dcc7fc50e22132b552f459d03f8c42f04dd62fea
12东永 <2808050775@qq.com>
2018-03-26 16:15:46 +0800

    待day20转换流

---
+++ Day20/src/com/mudy/transform/CopyDemo.java
@@ -0,0 +1,17 @@
+package com.mudy.transform;
+
+import java.io.*;
+
+/**
+ * Created by mudy on 18/3/26.
+ *
+ */
+public class CopyDemo {
+    public static void main(String[] args) throws Exception{
+        //1
+        File srcFile = new File("srcFile");
+        File desFile = new File("desFile");
+
+    }
+
+}
